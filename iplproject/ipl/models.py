c47714f3e816fcd2712d1d8ace48f020be678ac1
Mudassir Shaikh <mudassirs698@gmail.com>
2022-12-27 12:51:35 +0530

    added enviroment variable

--- iplproject/ipl/models.py
+++ iplproject/ipl/models.py
@@ -1,31 +1,33 @@
 from django.db import models
 
 # Create your models here.
+
+
 class Matches(models.Model):
 
     id = models.IntegerField(primary_key=True)
     season = models.IntegerField(null=True)
-    city = models.CharField(max_length=20,null=True)
+    city = models.CharField(max_length=20, null=True)
     date = models.DateField(null=True)
-    team1 = models.CharField(max_length=40,null=True)
-    team2 = models.CharField(max_length=40,null=True)
-    toss_winner = models.CharField(max_length=40,null=True)
-    toss_decision = models.CharField(max_length=40,null=True)
-    result = models.CharField(max_length=10,null=True)
+    team1 = models.CharField(max_length=40, null=True)
+    team2 = models.CharField(max_length=40, null=True)
+    toss_winner = models.CharField(max_length=40, null=True)
+    toss_decision = models.CharField(max_length=40, null=True)
+    result = models.CharField(max_length=10, null=True)
     dl_applied = models.IntegerField(null=True)
-    winner = models.CharField(max_length=40,null=True)
+    winner = models.CharField(max_length=40, null=True)
     win_by_runs = models.IntegerField(null=True)
     win_by_wickets = models.IntegerField(null=True)
-    player_of_match = models.CharField(max_length=30,null=True)
-    venue = models.CharField(max_length=80,null=True)
-    umpire1 = models.CharField(max_length=40,null=True)
-    umpire2 = models.CharField(max_length=40,null=True)
-    umpire3 = models.CharField(max_length=40,null=True)
+    player_of_match = models.CharField(max_length=30, null=True)
+    venue = models.CharField(max_length=80, null=True)
+    umpire1 = models.CharField(max_length=40, null=True)
+    umpire2 = models.CharField(max_length=40, null=True)
+    umpire3 = models.CharField(max_length=40, null=True)
 
 
 class Deliveries(models.Model):
-    
-    match_id= models.ForeignKey(Matches,on_delete=models.CASCADE)
+
+    match_id = models.ForeignKey(Matches, on_delete=models.CASCADE)
     inning = models.IntegerField()
     batting_team = models.CharField(max_length=100)
     bowling_team = models.CharField(max_length=100)
@@ -45,4 +47,4 @@ class Deliveries(models.Model):
     total_runs = models.IntegerField()
     player_dismissed = models.CharField(max_length=100)
     dismissal_kind = models.CharField(max_length=100)
-    fielder =models.CharField(max_length=100)
+    fielder = models.CharField(max_length=100)
--- iplproject/iplproject/settings.py
+++ iplproject/iplproject/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+from dotenv import load_dotenv
+
+load_dotenv() 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'fl=mjr3!j!d8r-daw!u05c*-*yoxbzyn--acgkr_tf7f4x^&p@'
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -80,7 +83,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'ipldb',
         'USER': 'ipluser',
-        'PASSWORD': '814353',
+        'PASSWORD': str(os.getenv('DATABASE_PASSWORD')),
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
